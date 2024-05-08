7e9640de754b7db95b49624b82437d1d1c0fa1e4
dudoserdkr <ivyur@knu.ua>
2024-05-08 17:16:39 +0300

    Refactoring 30% done, you can check it by git diff

--- PacMan.py
+++ PacMan.py
@@ -1,45 +1,47 @@
-from Field import FIELD
+from Field import FIELD, PACMAN_START_POS
 
 
 class PacMan:
 
-    START_POS = [23, 13]
-    PACMAN_CURRENT_STATUS = True  # current life True/False
-    AVAILABLE_LIVES = 3
+    START_POS = PACMAN_START_POS
 
-    def __init__(self):
-        self.position = self.START_POS.copy()
+    PACMAN_ALIVE = True
+    PACMAN_DEAD = False
 
-        self.condition = self.PACMAN_CURRENT_STATUS
+    def __init__(self) -> None:
+        self.position = self.START_POS
 
-        self.speed = 1
+        self.life_status = self.PACMAN_ALIVE
 
+        self.speed = 1  # influencing on how fast pacman image will be refreshing
         self.field = FIELD
 
-    def can_move_left(self):
-        curr_y, curr_x = self.position
-        if self.field[curr_y][curr_x - 1] == 1 or self.field[curr_y][curr_x - 1] == 2:
-            return False
-        return True
+        # self.availible_lifes = 3  Work in Progress for pacman alive status
 
-    def can_move_right(self):
+    def check_field_cell(self, dy: int, dx: int) -> bool:
         curr_y, curr_x = self.position
-        if self.field[curr_y][curr_x + 1] == 1 or self.field[curr_y][curr_x + 1] == 2:
-            return False
-        return True
+        return not (self.field[curr_y + dy][curr_x + dx] == 1 or self.field[curr_y + dy][curr_x + dx] == 2)
 
-    def can_move_up(self):
-        curr_y, curr_x = self.position
-        if self.field[curr_y - 1][curr_x] == 1 or self.field[curr_y - 1][curr_x] == 2:
-            return False
-        return True
+    # region move_allowers functions
 
-    def can_move_down(self):
-        curr_y, curr_x = self.position
-        if self.field[curr_y + 1][curr_x] == 1 or self.field[curr_y + 1][curr_x] == 2:
-            return False
-        return True
+    def can_move_left(self) -> bool:
+        dy, dx = 0, -1
+        return self.check_field_cell(dy, dx)
 
-    def death(self):
-        self.condition = False  # Means he is dead
-        self.position = self.START_POS
+    def can_move_right(self) -> bool:
+        dy, dx = 0, 1
+        return self.check_field_cell(dy, dx)
+
+    def can_move_up(self) -> bool:
+        dy, dx = -1, 0
+        return self.check_field_cell(dy, dx)
+
+    def can_move_down(self) -> bool:
+        dy, dx = 1, 0
+        return self.check_field_cell(dy, dx)
+
+    # endregion
+
+    # def death(self):
+    #     self.condition = False  # Means he is dead
+    #     self.position = self.START_POS
