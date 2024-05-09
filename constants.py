2b7501d68ff9cb80994910a939a027322f862079
bobser <144664653+bobser223@users.noreply.github.com>
2024-05-09 17:32:16 +0300

    changed constants and settings

--- constants.py
+++ constants.py
@@ -9,3 +9,5 @@ CELL_SIZE, DELAY = settings_arr
 
 ANIMATION_STEP_SIZE = 2
 ANIMATION_STEP = CELL_SIZE // ANIMATION_STEP_SIZE  # 10
+
+# Напишу українською, щоб всі зрозуміли, використовуємо CELL_SIZE, DELAY кратні 2
